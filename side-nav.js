20504430fc6f8ef56593f909c32c0c95eda9f91f
Kinga Żarkowska <kinga.zarkowska@gmail.com>
2019-07-31 12:47:14 +0200

    Add function to side-nav

--- side-nav.js
+++ side-nav.js
@@ -1,11 +1,44 @@
-let sideNavLi = document.querySelectorAll(".side-nav-container__li");
-let sideNavLiLine = document.querySelectorAll(".side-nav-container__li:hover:before");
+let sideNavLi = document.querySelectorAll(".side-nav-ul__a__li");
 
-const startSideNavLi = function() {
-    sideNavLi[0]classList.add('active-side-nav');
-    sideNavLiLine[0]classList.add('active-side-nav');
+const resetNavLi = function(){
+    for (let i = 0; i < sideNavLi.length; i++){
+        sideNavLi[i].style.opacity = 0.001;
+    }
 };
 
-// sideNavLi.addEventListener('click', function(){
+const startNavLi = function() {
+    resetNavLi();
+    sideNavLi[0].style.opacity = 1;
+};
 
-// }
+startNavLi();
+
+sideNavLi[0].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[0].style.opacity = 1;
+});
+
+sideNavLi[1].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[1].style.opacity = 1;
+});
+
+sideNavLi[2].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[2].style.opacity = 1;
+});
+
+sideNavLi[3].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[3].style.opacity = 1;
+});
+
+sideNavLi[4].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[4].style.opacity = 1;
+});
+
+sideNavLi[5].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[5].style.opacity = 1;
+});
