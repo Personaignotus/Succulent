ca858c71753232507c90472d4b7e1da19a56faff
Kinga Żarkowska <kinga.zarkowska@gmail.com>
2019-08-12 10:50:29 +0200

    Fix side-nav-menu

--- side-nav.js
+++ side-nav.js
@@ -1,4 +1,5 @@
 let sideNavLi = document.querySelectorAll(".side-nav-ul__li");
+let sideNavLiA = document.querySelectorAll(".side-nav-ul__li__a");
 
 const resetNavLi = function(){
     for (let i = 0; i < sideNavLi.length; i++){
@@ -6,27 +7,56 @@ const resetNavLi = function(){
     }
 };
 
-sideNavLi[0].addEventListener('click', function(){
+sideNavLiA[0].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[0].classList.add('active-side-nav');
 });
-sideNavLi[1].addEventListener('click', function(){
+sideNavLiA[1].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[1].classList.add('active-side-nav');
 });
-sideNavLi[2].addEventListener('click', function(){
+sideNavLiA[2].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[2].classList.add('active-side-nav');
 });
-sideNavLi[3].addEventListener('click', function(){
+sideNavLiA[3].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[3].classList.add('active-side-nav');
 });
-sideNavLi[4].addEventListener('click', function(){
+sideNavLiA[4].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[4].classList.add('active-side-nav');
 });
-sideNavLi[5].addEventListener('click', function(){
+sideNavLiA[5].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[5].classList.add('active-side-nav');
+});
+
+//
+
+let headerSideNavLi = document.querySelectorAll(".how-to-care__side-nav-li");
+
+headerSideNavLi[0].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[0].classList.add('active-side-nav');
+});
+headerSideNavLi[1].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[1].classList.add('active-side-nav');
+});
+headerSideNavLi[2].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[2].classList.add('active-side-nav');
+});
+headerSideNavLi[3].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[3].classList.add('active-side-nav');
+});
+headerSideNavLi[4].addEventListener('click', function(){
+    resetNavLi();
+    sideNavLi[4].classList.add('active-side-nav');
+});
+headerSideNavLi[5].addEventListener('click', function(){
     resetNavLi();
     sideNavLi[5].classList.add('active-side-nav');
 });
